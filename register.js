c8f677684d746058ffa7586a06aaa30e27e1b4c0
TAZANOU-DEV <nlstazan@gmail.com>
2025-01-15 13:39:32 +0100

    third commit

---
+++ register.js
@@ -0,0 +1,11 @@
+function regist(){
+    let ps = document.getElementById('pass');
+    let cps = document.getElementById('c_pass');
+
+    if(ps !== cps ){
+
+        event.preventDefault();
+alert("confirm same password");
+    }
+    
+}
