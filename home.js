0965387642be68697d67d1e452e9e6384b7c5176
TAZANOU-DEV <nlstazan@gmail.com>
2025-02-04 22:17:36 +0100

    7th commit

--- home.js
+++ home.js
@@ -13,8 +13,35 @@ imageinput.addEventListener('change',(e) => {
     reader.readAsDataURL(file);
 })
 
-function toggle_dropdown(){
 
+
+window.onload = function(){
+    const savedimage = localStorage.getItem("uploadedimage");
+    if(savedimage){
+
+        imagepreview.src = savedimage;
+        imagepreview.style.display = "block";
+    }
+};
+
+imageinput.addEventListener("change", function(event){
+    const file = event.target.addEventListener.files[0];
+    if(file){
+        const reader = new FileReader();
+        reader.onload = function(e){
+            imagepreview.src = e.target.result;
+            imagepreview.style.display = "block";
+            localStorage.setItem("uploadedimage",e.target.result);
+        };
+        reader.readAsDataURL(file);
+    }
+
+});
+
+
+
+function toggle_dropdown(){
+   
     var droppdown = document.getElementById('mydrop');
 
     if(droppdown.style.display === "block"){
@@ -24,15 +51,27 @@ function toggle_dropdown(){
         droppdown.style.display ="block";
     }
 
-    if (!event.target.matches('.yy')) {
-        var dropdowns = document.getElementsByClassName("drop_down");
-        for (var i = 0; i < dropdowns.length; i++) {
-            var openDropdown = dropdowns[i];
-            if (openDropdown.style.display === "block") {
-                openDropdown.style.display = "none";
-            }
-        }
+}
+
+function toggle_dropdown2(){
+
+    var body_right = document.getElementById('bd_r');
+    var droppdown = document.getElementById('mydrop');
+    var body_left = document.getElementById('bd_left');
+
+    if(body_left.style.display ==="none"){
+        body_left.style.display = "block";
+
+    }else{
+
+        body_left.style.display = "none";
+
     }
 
+    if( body_right.style.display === "block"){
+        body_right.style.display ="none";
 
+}else{
+    body_right.style.display ="block"
+}
 }
